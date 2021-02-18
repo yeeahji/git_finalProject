5dcefdeb5f75396fdd115ec84d3121a6e833c8fd
lgt <lkt01010@naver.com>
2021-02-19 00:35:21 +0900

    last commit

--- finalProject/src/main/webapp/admin/js/memberList.js
+++ finalProject/src/main/webapp/admin/js/memberList.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){//처음들어가자마자 테이블 출력
-   storeListPrint();
+$(document).ready(function() {//처음들어가자마자 테이블 출력
+   memberListPrint();
 });
 
 //selectPrint눌렀을때 (20개보기 50개보기..)
@@ -7,162 +7,63 @@ $('#selectPrint').change(function(){
    var viewNum = $(this).val();
    $('#viewNum').val(viewNum);
    
-   storeListPrint();
+   memberListPrint();
 });
 
-//상점리스트 출력
-function storeListPrint(){
+function memberListPrint(){
    $.ajax({
       type: 'get',
-      url: '/market/admin/getStoreList',
+      url: '/market/admin/getMemberList',
       data: {'pg': $('#pg').val(),
             'viewNum': $('#viewNum').val()},
       dataType: 'json',
       success: function(data){
          $("#tbody tr:gt(0)").remove();
-         $.each(data.storeList, function(index, items){
+         $.each(data.list, function(index, items){
+            var mem_enabled = items.enabled;
+            if(mem_enabled==='true'){
+               mem_enabled='정상';
+            }else if(mem_enabled==='false'){
+               mem_enabled='정지';
+            }
+            
             $('<tr/>').append($('<td/>',{
-                  text: items.mem_id,
-               })).append($('<td/>',{
-                  }).append($('<a/>',{
-                     href: '#',
-                  text: items.store_nickname,
-                  id: 'subjectA',
-                  class: items.seq+""
-                  }))
-                  ).append($('<td/>',{
-                     text: items.store_echo
-                  })).appendTo($('#tbody'));
+               text: items.mem_id
+            })).append($('<td/>',{
+            }).append($('<a/>',{
+               href: '#',
+               text: items.mem_name,
+               id: 'subjectA',
+               class: items.seq+""
+            }))
+            ).append($('<td/>',{
+               text: items.mem_email
+            })).append($('<td/>',{
+               text: mem_enabled
+            })).append($('<td/>',{
+               text: items.mem_add1
+            })).appendTo($('#tbody'));
          });//each
-         //페이징처리
-           $('#boardPagingDiv').html(data.adminStoreBP.pagingHTML);
          
-           //클릭_오른쪽에 정보나타나게
-           let id;
-           $('#storeTable').on('click','#subjectA',function(){
-              id = $(this).parent().prev().text();
-              $.ajax({
-                 type: 'get',
-                 url: '/market/admin/getStoreView',
-                 data: {'id': id},
-                 dataType: 'json',
-                 success: function(data){
-                    clickEvent(data);
-                 }
-              });
-          //판매중, 예약중, 판매완료 - 정렬
-          $('.btn-group>button').click(function(){
-             let product_manage= $(this).attr('value');
-             alert(product_manage);
-             $.ajax({
-                 type: 'get',
-                 url: '/market/admin/getStoreViewOrderby',
-                 data: {'id': id,
-                       'product_manage': product_manage},
-                 dataType: 'json',
-                 success: function(data){
-                    $("#store_product_tbody tr:gt(0)").remove();
-                    clickEvent_2(data);
-                 }
-              });
-          });
-         });
-      }
+         $('#totalMember').text(data.totalMember)
+         //페이징처리
+         $('#boardPagingDiv').html(data.adminBoardPaging.pagingHTML);
+         
+         //클릭
+         clickEvent(data);
+      }//success
    });
-}
-//클릭시 상세보기
-function clickEvent(data){
-    $('#nameSpan').text(data.adminMembersDTO.mem_name)
-    $('#storeNameSpan').text(data.adminMembersDTO.store_nickname)
-    if(data.adminMembersDTO.store_img != null){
-       $('#pictureSpan').text(data.adminMembersDTO.store_img)
-    }else{
-       $('#pictureSpan').text('사진없음')
-    }
-    $('#echoSpan').text(data.adminMembersDTO.store_echo)
-    $('#introSpan').text(data.adminMembersDTO.store_intro)
-    $('#reviewSpan').text(data.reviewTotalA)
-    $('#total_Product1Span').text(data.totalSellProduct)
-    $('#total_Product2Span').text(data.totalSellProduct)
-    
-    //상점이름 누르면 옆에 나오는 물품 테이블
-    $("#store_product_tbody tr:gt(0)").remove();
-    clickEvent_2(data);
-}
-//상세보기 아래에 있는 물품 테이블
-function clickEvent_2(data){
-   $.each(data.productList, function(index, items){
-       var product_manage = items.product_manage;
-       if(product_manage === 1){
-          product_manage = '판매중'
-       }else if(product_manage === 2){
-          product_manage = '예약중'
-       }else if(product_manage === 3){
-          product_manage = '판매완료'
-       }
-       
-       $('<tr/>').append($('<td/>',{
-       }).prepend($('<input/>',{
-            type: 'checkbox',
-            name: 'check',
-            value: items.product_seq
-         }))
-       ).append($('<td/>',{
-         text: items.product_seq
-       })).append($('<td/>',{
-       }).append($('<a/>',{
-         href: '#',
-         text: items.product_subject,
-         value: items.product_seq,
-         id: 'subjectA',
-         class: items.seq+""
-      }))
-       ).append($('<td/>',{
-          text: items.product_delivery_fee
-       })).append($('<td/>',{
-          text: items.product_price
-       })).append($('<td/>',{
-          text: product_manage
-       })).appendTo($('#store_product_tbody'));
-    });//each
-    
-   //전체 선택 또는 해제
-   $('#all').click(function(){
-      if($('#all').prop('checked'))
-         $('input[name=check]').prop('checked', true);
-      else
-         $('input[name=check]').prop('checked', false);
-   });
-    
-   //선택 삭제
-   $('#choiceDeleteBtn').click(function(){
-      let count = $('input[name=check]:checked').length;
-      
-      if(count == 0)
-         alert('삭제할 항목을 선택하세요');
-      else{
-         if(confirm('정말로 삭제하시겠습니까?')){
-            $('#store_productListForm').submit();
-         }
-      }
-   });
-    
-//글 클릭하면 넘어가게 할거임
-    $('#store_product_Table').on('click','#subjectA',function(){
-       let seq = $(this).parent().prev().text();
-      window.open("/market/product/productDetail?seq="+seq,"PopupWin","width=800,height=800");
-   });
-}
+};
 
 
 //조건검색======================================================
 
 //검색
-$('#storeSearchBtn').click(function(event, str){
+$('#memberSearchBtn').click(function(event, str){
    if(str != 'research'){
       $('input[name=searchPg]').val(1);
    }
-   if($('#storeKeyword').val() == ''){
+   if($('#keyword').val() == ''){
       alert('검색어를 입력하세요');
    }else{
       search_viewNum_change();
@@ -180,64 +81,140 @@ $('#storeSearchBtn').click(function(event, str){
 function search_viewNum_change(data){
    $.ajax({
       type: 'post',
-      url: '/market/admin/getSearchStoreList',
+      url: '/market/admin/getSearchMember',
       data: {'pg': $('#pg').val(),
             'searchType':$('#searchType').val(),
-            'storeKeyword':$('#storeKeyword').val(),
+            'keyword':$('#keyword').val(),
             'viewNum': $('#viewNum').val()},
       dataType: 'json',
       success: function(data){
          $("#tbody tr:gt(0)").remove();
-         //alert(JSON.stringify(data));
-         $.each(data.storeList, function(index, items){
+         $.each(data.list, function(index, items){
+            var mem_enabled = items.enabled;
+            if(mem_enabled.equals===true){
+               mem_enabled='정상'
+            }else if(mem_enabled.equals===false){
+               mem_enabled='정지'
+            }
+            
             $('<tr/>').append($('<td/>',{
-                  text: items.mem_id,
-               })).append($('<td/>',{
-                  }).append($('<a/>',{
-                     href: '#',
-                  text: items.store_nickname,
-                  id: 'subjectA',
-                  class: items.seq+""
-                  }))
-                  ).append($('<td/>',{
-                     text: items.store_echo
-                  })).appendTo($('#tbody'));
+               text: items.mem_id
+            })).append($('<td/>',{
+            }).append($('<a/>',{
+               href: '#',
+               text: items.mem_name,
+               id: 'subjectA',
+               class: items.seq+""
+            }))
+            ).append($('<td/>',{
+               text: items.mem_email
+            })).append($('<td/>',{
+               text: mem_enabled
+            })).append($('<td/>',{
+               text: items.mem_add1
+            })).appendTo($('#tbody'));
          });//each
-         //페이징처리
-           $('#boardPagingDiv').html(data.adminStoreBP.pagingHTML);
          
-//           //클릭
-           $('#storeTable').on('click','#subjectA',function(){
-              let id = $(this).parent().prev().text();
-              $.ajax({
-                 type: 'get',
-                 url: '/market/admin/getStoreView',
-                 data: {'id': id},
-                 dataType: 'json',
-                 success: function(data){
-                    clickEvent(data);
-                 }
-              });
-           });
+         //페이징처리
+           $('#boardPagingDiv').html(data.adminBoardPaging.pagingHTML);
+           
+           //클릭
+           clickEvent(data);
       }
-   });//ajax
+   });
 }
+
+
+//클릭후 나오는 공통정보====================================================
+
+
+function clickEvent(data){
+   let id;
+    $('#memberTable').on('click','#subjectA',function(){
+         id = $(this).parent().prev().text();
+         $.ajax({
+            type: 'post',
+            url: '/market/admin/memberView',
+            data: {'id': id},
+            dataType: 'json',
+            success: function(data){
+               //alert(JSON.stringify(data));
+               showMember(data);
+            }
+         });
+      });
+      
+      $('#moveStorePageBtn').click(function(){
+         window.open("/market/store/store?id="+id,"PopupWin","width=800,height=800");
+      });
+      
+      
+      //영구정지
+      $('#memberBlockBtn').click(function(){
+           //window.confirm(id+'님을 영구정지 하시겠습니까?');
+         var result = confirm(id+'님을 영구정지 하시겠습니까?');
+         
+         if(result){
+            $.ajax({
+               type: 'get',
+               url: '/market/admin/memberBlock',
+               data: {'id':id},
+               success: function(){
+                  alert('계정을 정지했습니다.');
+               },
+               error: function(){
+                  alert(id+'님의 계정을 영구정지 했습니다.');
+                  location.reload();
+               }
+            });
+         }else{
+            
+         }
+        });
+      //영구정지해제
+      $('#memberReleaseBtn').click(function(){
+         //window.confirm(id+'님을 영구정지 하시겠습니까?');
+         var result = confirm(id+'님의 영구정지를 해제 하시겠습니까?');
+         
+         if(result){
+            $.ajax({
+               type: 'get',
+               url: '/market/admin/memberReleaseBtn',
+               data: {'id':id},
+               success: function(){
+                  alert('계정을 복구했습니다.');
+               },
+               error: function(err){
+                  alert(id+'님의 계정을 복구 했습니다.');
+                  location.reload();
+               }
+            });
+         }else{
+            
+         }
+      });
+   
+}
+
 
 //상세정보에 나타나는 부분
 function showMember(data){
-	$('#nameSpan').text(data.adminMembersDTO.mem_name)
-	$('#HpSpan').text(data.adminMembersDTO.mem_tel)
-	$('#enabledSpan').text(data.adminMembersDTO.enabled)
-	$('#add1Span').text(data.adminMembersDTO.mem_add1)
-	$('#add2Span').text(data.adminMembersDTO.mem_add2)
-	$('#store1NameSpan').text(data.adminMembersDTO.store_nickname)
-	$('#store2NameSpan').text(data.adminMembersDTO.store_nickname)
-	$('#echo1Span').text(data.adminMembersDTO.store_echo)
-	$('#echo2Span').text(data.adminMembersDTO.store_echo)
-	$('#emailSpan').text(data.adminMembersDTO.mem_email)
-	$('#logSpan').text(data.adminMembersDTO.mem_logtime)
-	$('#productSellSpan').text(data.totalSellProduct)
-	$('#productBuySpan').text(data.totalBuyProduct)
-	$('#reportedSpan').text(data.totalReported)
+   $('#nameSpan').text(data.adminMembersDTO.mem_name)
+   $('#HpSpan').text(data.adminMembersDTO.mem_tel)
+   $('#enabledSpan').text(data.adminMembersDTO.enabled)
+   $('#add1Span').text(data.adminMembersDTO.mem_add1)
+   $('#add2Span').text(data.adminMembersDTO.mem_add2)
+   $('#store1NameSpan').text(data.adminMembersDTO.store_nickname)
+   $('#store2NameSpan').text(data.adminMembersDTO.store_nickname)
+   $('#echo1Span').text(data.adminMembersDTO.store_echo)
+   $('#echo2Span').text(data.adminMembersDTO.store_echo)
+   $('#emailSpan').text(data.adminMembersDTO.mem_email)
+   $('#logSpan').text(data.adminMembersDTO.mem_logtime)
+   $('#productSellSpan').text(data.totalSellProduct)
+   $('#productBuySpan').text(data.totalBuyProduct)
+   $('#reportedSpan').text(data.totalReported)
 };
 
+
+
+ 
